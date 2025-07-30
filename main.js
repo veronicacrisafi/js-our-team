033db05d6e0cb724f0ea9278574c3f253d87e612
veronicacrisafi <veronicacrisafi3@gmail.com>
2025-07-30 21:36:01 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,55 @@
+//document.writeln("hi");
+/* Dato un array di oggetti rappresentante un team di un’azienda,
+ creare una pagina dedicata  in cui mostrare una card per ciascun componente.
+
+(trovate l’array del team all’interno della cartella in allegato)
+
+Bonus
+
+Rendere l’esercizio responsive, mandando a capo le card
+Aggiungere un form di agginta membri che permetta di visualizzare il nuovo membro 
+sulla pagina (usate una foto qualunque, anche vostra se volete sentirvi parte del team! 😀) */
+
+// seleziono i nodi della dom
+const rowEl = document.getElementById("my-team");
+
+const teamMembers = [
+  {
+    name: "Marco Bianchi",
+    role: "Designer",
+    email: "marcobianchi@team.com",
+    img: "img/male1.png",
+  },
+  {
+    name: "Laura Rossi",
+    role: "Front-end Developer",
+    email: "laurarossi@team.com",
+    img: "img/female1.png",
+  },
+  {
+    name: "Giorgio Verdi",
+    role: "Back-end Developer",
+    email: "giorgioverdi@team.com",
+    img: "img/male2.png",
+  },
+  {
+    name: "Marta Ipsum",
+    role: "SEO Specialist",
+    email: "martarossi@team.com",
+    img: "img/female2.png",
+  },
+  {
+    name: "Roberto Lorem",
+    role: "SEO Specialist",
+    email: "robertolorem@team.com",
+    img: "img/male3.png",
+  },
+  {
+    name: "Daniela Amet",
+    role: "Analyst",
+    email: "danielaamet@team.com",
+    img: "img/female3.png",
+  },
+];
+
+//ciclo nell'array e prendo il singolo elemento
