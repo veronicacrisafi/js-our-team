857c13467ddb0cc566393ee98e33800f37627795
veronicacrisafi <veronicacrisafi3@gmail.com>
2025-07-31 17:34:10 +0200

    destrutturato l'oggetto
    creato cardmarkup con all'interno la stringa e inserite le key dell'oggetto
    
    inserito in pagina cardMarkup

--- main.js
+++ main.js
@@ -56,7 +56,38 @@ const teamMembers = [
 for (let i = 0; i < teamMembers.length; i++) {
   const thisMember = teamMembers[i];
   console.log(thisMember.name);
+  //destrutturazione dell'oggetto dentro il ciclo for della nuova variabile thisMember
+  const { name, role, email, img } = thisMember;
+  console.log(name);
+  console.log(role);
+  console.log(email);
+  console.log(img);
+  // generiamo il markup della card
+  const cardMarkupString = `  
+ <div class="col">
+    <div class="card mb-3 bg-dark" style="max-width: 540px;">
+        <div class="row g-0">
+            <div class="col-md-4">
+                <img src="${img}" class="img-fluid rounded" alt="img-first-member">
+            </div>
+                <div class="col-md-8 pt-2">
+                    <div class="card-body">
+                        <h5 id="name-member" class="card-title fw-bold text-light">${name}</h5>
+                            <div id="role-member" class="pt-2 text-light fw-bold">
+                            ${role}
+                            </div>
+                                <div id="mail-member" class="pt-2 fw-bold text-info">
+                                ${email}
+                                </div>
+                    </div>
+                </div>
+            </div>
+    </div> `;
+
+  console.log(cardMarkupString);
+
+  rowEl.insertAdjacentHTML("beforeend", cardMarkupString);
 }
+
 // creare le variabili per conservare i valori di ogni proprietà
-// generiamo il markup
 // creato il markup lo devo appendere alla dom
